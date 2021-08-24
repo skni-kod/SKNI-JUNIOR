a59f0f3cacd1957d10e503cee864bbe372a98c5e
Hubert Futoma <futomahubert@gmail.com>
2021-08-24 13:01:46 +0200

    Team-icons

--- src/components/Team.vue
+++ src/components/Team.vue
@@ -11,21 +11,42 @@
         <v-card class="col-lg-12 col-xl-12 outline rounded-lg mr-2 mb-2">
           <div data-aos="zoom-in">
             <div class="d-flex justify-center mb-3">
-              <img :src="require('@/assets/team-3.jpg')" class="img-fluid" style="width: 20vw; min-width: 150px; height: auto;" />
+              <img
+                :src="require('@/assets/team-3.jpg')"
+                class="img-fluid"
+                style="width: 20vw; min-width: 150px; height: auto;"
+              />
             </div>
             <div>
               <h4>{{ mentor.name }}</h4>
               <h5 class="green--text mb-0 p-0">{{ mentor.position }}</h5>
               <div class="my-0 ml-0 d-flex justify-center">
-                <v-col cols="6" lg="9" sm="3" class="d-flex justify-center p-1 pl-0 mb-2">
-                  <v-btn icon color="#4267B2" :href="mentor.fb" class="text-decoration-none">
-                    <v-icon>mdi-facebook </v-icon>
+                <v-col cols="6" class="d-flex justify-center p-1 pl-0 mb-2">
+                  <v-btn
+                    icon
+                    color="#4267B2"
+                    :href="mentor.fb"
+                    class="text-decoration-none"
+                    style="margin-right: 0.8vw"
+                  >
+                    <v-icon size="36">mdi-facebook </v-icon>
                   </v-btn>
-                  <v-btn icon color="#0e76a8" :href="mentor.link" class="text-decoration-none">
-                    <v-icon>mdi-linkedin </v-icon>
+                  <v-btn
+                    icon
+                    color="#0e76a8"
+                    :href="mentor.link"
+                    class="text-decoration-none"
+                    style="margin-right: 0.8vw"
+                  >
+                    <v-icon size="36">mdi-linkedin </v-icon>
                   </v-btn>
-                  <v-btn icon color="black" :href="mentor.git" class="text-decoration-none">
-                    <v-icon>mdi-github </v-icon>
+                  <v-btn
+                    icon
+                    color="black"
+                    :href="mentor.git"
+                    class="text-decoration-none"
+                  >
+                    <v-icon size="36">mdi-github </v-icon>
                   </v-btn>
                 </v-col>
               </div>
